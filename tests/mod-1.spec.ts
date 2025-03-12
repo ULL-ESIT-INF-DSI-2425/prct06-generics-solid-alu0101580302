d252340d45b1f259fcca2de6e876dbe444147f1c
Maya Santana Amador <alu0101580302@ull.edu.es>
2025-03-12 11:04:50 +0000

    Más pruebas de la modificación

--- tests/mod-1.spec.ts
+++ tests/mod-1.spec.ts
@@ -15,6 +15,14 @@ describe("Pruebas de ComplexNumber", () => {
         test("(-2+4i) + (7+i)", () => {
             expect(num3.add(num4)).toStrictEqual(new ComplexNumber(5, 5));
         });
+
+        test("(1+2i) + (-2+4i)", () => {
+            expect(num1.add(num3)).toStrictEqual(new ComplexNumber(-1, 6));
+        });
+
+        test("(1+2i) + (7+i)", () => {
+            expect(num1.add(num4)).toStrictEqual(new ComplexNumber(8, 3));
+        });
     });
 
     describe("Pruebas de substract", () => {
@@ -24,6 +32,14 @@ describe("Pruebas de ComplexNumber", () => {
     
         test("(3-5i) - (-2+4i)", () => {
             expect(num2.substract(num3)).toStrictEqual(new ComplexNumber(5, -9));
+        });
+
+        test("(1+2i) - (-2+4i)", () => {
+            expect(num1.substract(num3)).toStrictEqual(new ComplexNumber(3, -2));
+        });
+
+        test("(1+2i) - (7+i)", () => {
+            expect(num1.substract(num4)).toStrictEqual(new ComplexNumber(-6, 1));
         });
     });
 
@@ -35,6 +51,14 @@ describe("Pruebas de ComplexNumber", () => {
         test("(1+2i) * (7+i)", () => {
             expect(num1.multiply(num4)).toStrictEqual(new ComplexNumber(5, 15));
         });
+
+        test("(1+2i) * (-2+4i)", () => {
+            expect(num1.multiply(num3)).toStrictEqual(new ComplexNumber(-10, 0));
+        });
+
+        test("(3-5i) * (-2+4i)", () => {
+            expect(num2.multiply(num3)).toStrictEqual(new ComplexNumber(14, 22));
+        });
     });
 
     describe("Pruebas de divide", () => {
@@ -44,6 +68,14 @@ describe("Pruebas de ComplexNumber", () => {
     
         test("(3-5i) / (7+i)", () => {
             expect(num2.divide(num4)).toStrictEqual(new ComplexNumber(8/25, -19/25));
+        });
+
+        test("(1+2i) / (-2+4i)", () => {
+            expect(num1.divide(num3)).toStrictEqual(new ComplexNumber(3/10, -2/5));
+        });
+
+        test("(1+2i) / (7+i)", () => {
+            expect(num1.divide(num4)).toStrictEqual(new ComplexNumber(9/50, 13/50));
         });
     });
 });
@@ -60,9 +92,21 @@ describe("Pruebas de ArithmeticableCollection", () => {
         test("", () => {
             expect(collection1.getArithmeticable(2)).toStrictEqual(num3);
         });
+
+        test("", () => {
+            expect(collection2.getArithmeticable(0)).toStrictEqual(num1);
+        });
+
+        test("", () => {
+            expect(collection2.getArithmeticable(2)).toStrictEqual(num4);
+        });
     
         test("", () => {
             expect(() => collection1.getArithmeticable(-1)).toThrowError("El índice se sale del array");
+        });
+
+        test("", () => {
+            expect(() => collection1.getArithmeticable(11)).toThrowError("El índice se sale del array");
         });
     });
 
@@ -84,6 +128,21 @@ describe("Pruebas de ArithmeticableCollection", () => {
             const num5: ComplexNumber = new ComplexNumber(5, 7);
             collection1.addArithmeticable(num5);
             expect(collection1.getArithmeticable(5)).toStrictEqual(num5);
+        });
+
+        test("", () => {
+            expect(collection1.getNumberOfArithmeticables()).toStrictEqual(6);
+        });
+
+        test("", () => {
+            const num6: ComplexNumber = new ComplexNumber(5, 7);
+            collection2.addArithmeticable(num6);
+            expect(collection2.getArithmeticable(3)).toStrictEqual(num6);
+        });
+
+        test("", () => {
+            collection2.addArithmeticable(new ComplexNumber(5, 7));
+            expect(collection2.getNumberOfArithmeticables()).toStrictEqual(5);
         });
     });
 });
